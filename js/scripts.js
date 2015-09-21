608abfb8a8e123345634ad09b7b61ebe85f35d00
Archana Bongale & Molly Waggett <test@test.com>
2015-09-21 09:51:42 -0700

    Add form for user to enter info about places they've been and list all locations.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,17 @@
-// var functionName = function(params) {
-//
-// };
+$(document).ready(function() {
+  $("form#new-place").submit(function(event) {
+    event.preventDefault();
 
-// $(document).ready(function() {
-//
-// });
+    var newLocation = $("input#new-location").val();
+    var newLandmark = $("input#landmarks").val();
+    var timeOfYear = $("input#timeofyear").val();
+    var newPlace = { location: newLocation, landmarks: newLandmark, timeOfYear: timeOfYear };
+
+    $("ul#places").append("<li><span class='clickable'>" + newPlace.location + "</span></li>");
+    $("input#new-location").val("");
+    $("input#landmarks").val("");
+    $("input#timeofyear").val("");
+
+    $(".result").show();
+  });
+});
