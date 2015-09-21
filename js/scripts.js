45531a4c5cebd9d3dad848a052e82a6249172018
Archana Bongale & Molly Waggett <test@test.com>
2015-09-21 11:13:51 -0700

    Allow user to add multiple landmarks to a single location.

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,37 @@
 $(document).ready(function() {
+  $("#add-landmark").click(function() {
+    $(".form-group").append('<div class="addLandmark">'+
+                              '<label for = "landmarks">Add a landmark:</label>' +
+                              '<input type="text" class="form-control landmarks">' +
+                            '</div>');
+  });
+
   $("form#new-place").submit(function(event) {
     event.preventDefault();
 
     var newLocation = $("input#new-location").val();
-    var newLandmark = $("input#landmarks").val();
     var timeOfYear = $("input#timeofyear").val();
-    var newPlace = { location: newLocation, landmarks: newLandmark, timeOfYear: timeOfYear };
+    var newPlace = { location: newLocation, landmarks: [] , timeOfYear: timeOfYear };
+
+      $(".addLandmark").each(function() {
+        var newLandmark = $(this).find("input.landmarks").val();
+        newPlace.landmarks.push(newLandmark);
+      });
+
 
     $("ul#places").append("<li><span class='clickable'>" + newPlace.location + "</span></li>");
     $("input#new-location").val("");
-    $("input#landmarks").val("");
     $("input#timeofyear").val("");
+    $("input.landmarks").val("");
 
     $(".clickable").last().click(function() {
       $("#show-place").show();
       $("#show-place h2").text(newPlace.location);
-      $(".landmark").text(newPlace.landmarks);
       $(".timeofyear").text(newPlace.timeOfYear);
-
+      $("ul.landmark").text("");
+      newPlace.landmarks.forEach(function(landmark) {
+        $(".landmark").append("<li>"+landmark+"</li>");
+      });
     });
 
     $(".result").show();
